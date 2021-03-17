9fd7578425043d88266d56dafac0cd4cbcc0e636
MHShayek <mahedihassanshayek@gmail.com>
2021-03-17 18:51:21 +0600

    private route done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBWXUmQAniCFUoJvQQi6AkY5U5KvD3nGEc",
+    authDomain: "burj-al-arab-70c6a.firebaseapp.com",
+    projectId: "burj-al-arab-70c6a",
+    storageBucket: "burj-al-arab-70c6a.appspot.com",
+    messagingSenderId: "671536044236",
+    appId: "1:671536044236:web:7f86d2ee5b611ebde54a69",
+};
+export default firebaseConfig;
